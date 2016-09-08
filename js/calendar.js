b5bc9bd52350e25e7ef547b286c23b06c206f58e
Judy Wu <judy.wu@meginfo.com>
2016-09-08 16:00:38 +0800

    初始化日历页面

---
+++ js/calendar.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var calendarDaysArray = $('.calendar-days td').toArray();
+
+	var currentDate = new Date();
+	var currentYear = currentDate.getFullYear();
+	var currentMonth = currentDate.getMonth();
+	var currentDay = currentDate.getDate();
+	var currentDayOfWeek = currentDate.getDay();
+
+
+});
