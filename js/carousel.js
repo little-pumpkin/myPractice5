e43ae41a206f847797e450251f1d49cc79eed042
Judy Wu <judy.wu@meginfo.com>
2016-09-12 10:29:47 +0800

    调整轮播样式

--- js/carousel.js
+++ js/carousel.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
 	var last = $('.carousel li').last();
 	var liWidth = first.width();
 
-  carousel = $('.carousel ul');
+  var carousel = $('.carousel ul');
   carousel.prepend(last.clone()).append(first.clone());
   carousel.width(liWidth * $('.carousel li').length);
   carousel.css({left: -liWidth});
 
   $('.prev').on('click', function() {
     carousel.animate({left: '+=' + liWidth}, 500, function(){
+    	// position()函数用于返回当前匹配元素相对于其被定位的祖辈元素的偏移，
+    	// 也就是相对于被定位的祖辈元素的坐标。该函数只对可见元素有效。
       if(Math.abs(carousel.position().left) < 2) {
         carousel.css({left: -liWidth * (carousel.children().length - 2)});
       }
